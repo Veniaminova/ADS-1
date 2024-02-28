c3ec534228c8b171eb10c65e76d3991ffb82dd4c
Анастасия <98629650+Veniaminova@users.noreply.github.com>
2024-02-28 13:56:30 +0300

    alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,20 +1,59 @@
 // Copyright 2022 NNTU-CS
 #include <cstdint>
 #include "alg.h"
+#include <cmath>
 
-
-bool checkPrime(uint64_t value) {
-// вставьте код функции
+bool chekPrime(uint64_t value) {
+	for (int i = 2; i <= sqrt(value); ++i) {
+		if (value % i == 0)
+			return false;
+	}
+	return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+	uint64_t count = 0;
+	uint64_t num = 2;
+		while (count < n) {
+		if (chekPrime(num)) {
+			count++;
+		}
+		num++;
+	}
+	cout << num - 1;
+	return num - 1;
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+	int nextPrime = value + 1;
+	bool isPrime = true;
+	while (isPrime) {
+		int isPrime = true;
+		for (int i = 2; i <= sqrt(nextPrime); i++) {
+			if (nextPrime % i == 0) {
+				isPrime = false;
+				break;
+			}
+		}
+		if (isPrime) {
+			cout << "next prime " << nextPrime;
+			return nextPrime;
+		}
+		else {
+			nextPrime++;
+		}
+	}
 }
 
+
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+	int summa = 0;
+	for (int i = 0; i < hbound; ++i) {
+		for (int n = 2; n < sqrt(i); ++n) {
+			if (i % n != 0)
+				summa += i;
+		}
+	}
+	cout << "sum prime " << summa;
+	return summa;
 }
