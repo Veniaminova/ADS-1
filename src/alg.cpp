63b1bf47934cbc770bbab1be44f9c1a6e7fac216
Анастасия <98629650+Veniaminova@users.noreply.github.com>
2024-02-29 17:28:07 +0300

    alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,51 +3,41 @@
 #include <cmath>
 
 bool chekPrime(uint64_t value) {
-    for (int i = 2; i <= sqrt(value); ++i) {
-	if (value % i == 0)
-	    return false;
-    }
-    return true;
+	for (int i = 2; i*i <= value; ++i) {
+		if (value % i == 0){
+			return false;
+		}
+	}
+	return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-    uint64_t count = 0;
-    uint64_t num = 2;
-    while (count < n) {
-	if (chekPrime(num)) 
-	    count++;
-    }
-    num++;
-    return num - 1;
+    	uint64_t count = 0;
+   	uint64_t num = 2;
+    	while (count < n) {
+		if (chekPrime(num)){ 
+	    		count++;
+		}
+    	}
+    	num++;
+    	return num - 1;
 }
-
 uint64_t nextPrime(uint64_t value) {
-    int nextPrime = value + 1;
-    bool isPrime = true;
-    while (isPrime) {
-	int isPrime = true;
-	for (int i = 2; i <= sqrt(nextPrime); i++) {
-	    if (nextPrime % i == 0) {
-		isPrime = false;
-		break;
-	    }
+	uint64_t num = value + 1;
+	while (true) {
+		if (checkPrime(num)) {
+			return num;
+		}
+		num++;
 	}
-	if (isPrime) {
-	    return nextPrime;
-	}
-	else {
-	    nextPrime++;
-	}
-    }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-    int summa = 0;
-    for (int i = 0; i < hbound; ++i) {
-	for (int n = 2; n < sqrt(i); ++n) {
-	    if (i % n != 0)
-		summa += i;
+    	uint64_t sum = 0;
+	for (uint64_t i = 2; i < hbound; ++i) {
+		if (checkPrime(i)) {
+			sum += i;
+		}
 	}
-    }
-    return summa;
+	return sum;
 }
